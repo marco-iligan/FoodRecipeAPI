36166ac35a0821acf281673b5f001269af1b5ac3
marco-iligan <marco.iligan@accenture.com>
2023-03-20 16:22:30 +0800

    Initial Commit

---
+++ food-recipe/src/test/java/com/foodrecipe/api/FoodRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodrecipe.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
