fe67bb6af22292c39c5a392ddd73353987a245b5
marco-iligan <marco.iligan@accenture.com>
2023-03-24 11:04:55 +0800

    Added Swagger UI for REST API Documentation

--- src/main/java/com/foodrecipe/api/FoodRecipeApplication.java
+++ src/main/java/com/foodrecipe/api/FoodRecipeApplication.java
@@ -1,8 +1,30 @@
 package com.foodrecipe.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Food Recipe API",
+				version = "1.0.0",
+				description = "This is for learning only",
+				contact = @Contact(
+						name = "Marco Iligan",
+						email = "miligan17@gmail.com"
+				)
+		)
+)
+@SecurityScheme(
+		name = "token",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		scheme = "bearer"
+)
 @SpringBootApplication
 public class FoodRecipeApplication {
 
